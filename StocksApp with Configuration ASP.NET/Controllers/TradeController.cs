d9a70e59aefc363a2c300236936339fbb14285f6
nic00la <nic00la.kaleta@gmail.com>
2024-06-24 17:13:37 +0200

    Create Basic App

---
+++ StocksApp with Configuration ASP.NET/Controllers/TradeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StocksApp_with_Configuration_ASP.NET.Controllers;
+
+public class TradeController : Controller
+{
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
